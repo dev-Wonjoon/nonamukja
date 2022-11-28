899b94de5a3c18981c4fb05b0d2187d35ce98a0c
dev-Wonjoon <dnjswns775@gmail.com>
2022-11-28 12:33:55 +0000

    ADD: simplejwt

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
     path('signup', views.SignupView.as_view(), name="login"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
