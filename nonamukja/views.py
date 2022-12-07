f0f33d60b90c3d50496f39ef6686ec9a5a927e07
dev-Wonjoon <dnjswns775@gmail.com>
2022-12-07 02:53:03 +0000

    Get post user email update

--- nonamukja/views.py
+++ nonamukja/views.py
@@ -3,8 +3,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .models import Post
+from accounts.models import User
 from .serializers import PostSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.authentication import JWTStatelessUserAuthentication
+from rest_framework_simplejwt.models import TokenUser
 
 class PostListCreateView(APIView):
 
@@ -17,10 +20,14 @@ class PostListCreateView(APIView):
         header = JWTAuthentication.get_header(self, request=request)
         token = JWTAuthentication.get_raw_token(self, header)
         validated_token = JWTAuthentication.get_validated_token(self, token)
-        user = JWTAuthentication.get_user(self, validated_token)
-        print(user)
-        serializer = PostSerializer(request.data)
+        user_email = JWTStatelessUserAuthentication.get_user(self, validated_token)
+        print(type(user_email.pk))
+        user = User.objects.get(email=user_email.pk)
+
+        serializer = PostSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
